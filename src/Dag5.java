314b5bee0e9f699f1ea78e8332e7d3bb8900f0f6
h678159 <astridfrebja@gmail.com>
2026-01-08 12:40:46 +0100

    oppdatert dag 5

--- src/Dag5.java
+++ src/Dag5.java
@@ -1,5 +1,15 @@
 package src;
 
+import java.io.IOException; 
+import java.util.List; 
+import java.nio.file.Path; 
+import java.nio.file.Files; 
+
 public class Dag5 {
+
+    public static void main (String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("input_dag5.txt"));
+
+    }
     
 }
